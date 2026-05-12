08975898633f003a362fdac286dbdb08ad16a0ed
Luka <lukaberlec25@gmail.com>
2026-05-12 15:29:23 +0200

    fix: prisma client import

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,17 +1,13 @@
-import { Pool } from "pg";
-import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-if (!globalForPrisma.prisma) {
-  const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false },
-    client_encoding: "utf8",
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["query"],
   });
-  const adapter = new PrismaPg(pool);
-  globalForPrisma.prisma = new PrismaClient({ adapter });
-}
 
-export const prisma = globalForPrisma.prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
